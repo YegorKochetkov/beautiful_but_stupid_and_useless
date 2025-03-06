6fe1a161f2dd5f0aca92fcd6167dd9a2daa33421
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2025-03-06 22:59:30 +0200

    refactor(About): brake down elements to subcomponents for better representation

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -32,6 +32,13 @@ export function About() {
 
 	return (
 		<div id="about" className="w-screen min-h-screen bg-bbsu-blue-75">
+			<AboutContent />
+			<AboutBackground />
+		</div>
+	);
+
+	function AboutContent() {
+		return (
 			<div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
 				<h2 className="text-xl md:text-3xl xl:text-4xl px-4 mb-5 uppercase text-center">
 					Why don&apos;t &rdquo;pretty&ldquo; websites actually sell?
@@ -45,33 +52,37 @@ export function About() {
 
 				<div className="absolute -bottom-[80dvh] w-full max-w-96 text-center font-circular-web text-xl md:max-w-[34rem] md:text-2xl z-30">
 					<p>
-						Practice shows that beautiful design solutions do not always bring in
-						more sales than simply convenient, fast, and functional ones.
+						Practice shows that beautiful design solutions do not always bring
+						more sales than just convenient, fast and functional.
 					</p>
 
 					{/* <p className="text-gray-500">
-						Дорогие сайты кучей эффектов и анимаций могут быть неэффективными
-						из-за долгой загрузки и низкой конверсии. Идеальный путь клиента: пять
-						секунд на сайте и целевое действие. Реальный путь клиента более
-						длинный, c несколькими уровнями прогрева.
-					</p>
-					<p>
-						Цель сайта - донести информацию o продукте и получить деньги клиента.
-						Пользователь должен понимать, куда он попал, за три-пять секунд. Текст
-						на сайте должен быть понятным и отвечать на вопросы пользователя.
-						Важно сочетать информативность и визуальную привлекательность. Важно
-						знать свою целевую аудиторию и их потребности.
-					</p>
-					<h2>Социальные доказательства </h2>
-					<p>
-						Такие вещи как отзывы и рейтинги, возможность связаться c реальными
-						людьми для подтверждения качества продукта важны для доверия.
-						Упрощение сайта и добавление &rdquo;социальных доказательств&ldquo;
-						улучшает конверсию.
-					</p> */}
+            Дорогие сайты кучей эффектов и анимаций могут быть неэффективными
+            из-за долгой загрузки и низкой конверсии. Идеальный путь клиента: пять
+            секунд на сайте и целевое действие. Реальный путь клиента более
+            длинный, c несколькими уровнями прогрева.
+        </p>
+        <p>
+            Цель сайта - донести информацию o продукте и получить деньги клиента.
+            Пользователь должен понимать, куда он попал, за три-пять секунд. Текст
+            на сайте должен быть понятным и отвечать на вопросы пользователя.
+            Важно сочетать информативность и визуальную привлекательность. Важно
+            знать свою целевую аудиторию и их потребности.
+        </p>
+        <h2>Социальные доказательства </h2>
+        <p>
+            Такие вещи как отзывы и рейтинги, возможность связаться c реальными
+            людьми для подтверждения качества продукта важны для доверия.
+            Упрощение сайта и добавление &rdquo;социальных доказательств&ldquo;
+            улучшает конверсию.
+        </p> */}
 				</div>
 			</div>
+		);
+	}
 
+	function AboutBackground() {
+		return (
 			<div id="clip-image" className="h-dvh w-screen">
 				<div className="mask-clip-path absolute left-1/2 top-0 z-20 h-[60vh] w-96 origin-center -translate-x-1/2 overflow-hidden rounded-3xl md:w-[30vw];">
 					<img
@@ -81,6 +92,6 @@ export function About() {
 					/>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 }
