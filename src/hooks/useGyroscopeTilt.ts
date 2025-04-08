29ab4986b79b364ff5ac9a0469dffb6414ee14a2
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2025-04-09 01:16:37 +0300

    test: use alpha dimension instead gamma

--- src/hooks/useGyroscopeTilt.ts
+++ src/hooks/useGyroscopeTilt.ts
@@ -13,19 +13,19 @@ export const useGyroscopeTilt = (targetRef: React.RefObject<HTMLDivElement>) => 
 		targetRef.current.style.setProperty("--slide-transition-duration", "0ms");
 
 		const handleDeviceOrientation = (event: DeviceOrientationEvent) => {
-			if (event.beta !== null && event.gamma !== null) {
+			if (event.alpha !== null && event.gamma !== null) {
 				// Normalize and limit the tilt angles
-				const xRotation = Math.max(Math.min(event.beta, 21), -21);
-				const yRotation = Math.max(Math.min(event.gamma, 16), -16);
-
-				rotation.target.set(-xRotation, yRotation);
+				const xRotation = Math.max(Math.min(event.alpha, 8), 8);
+				const yRotation = Math.max(Math.min(event.gamma, 8), -8);
+				
+				rotation.target.set(xRotation, -yRotation);
 				rotation.current.interpolate(rotation.target, lerpFactor);
 
 				targetRef.current?.style.setProperty(
 					"transform",
-					`rotateX(${rotation.current.x.toFixed(2)}deg) 
-					rotateY(${rotation.current.y.toFixed(2)}deg)
-					perspective(800px)`
+					`rotateX(${rotation.current.x.toFixed(4)}deg) 
+					rotateY(${rotation.current.y.toFixed(4)}deg)
+					perspective(400px)`
 				);
 				targetRef.current?.style.setProperty(
 					"--slide-transition-duration",
